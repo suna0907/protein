16e53e257b2b7397ecbbe5853236bfca87e825f3
HIRAGA <fuya_hiraga@totec.co.jp>
2023-05-31 14:08:14 +0900

    okkkkk

--- myprotein/photo/views.py
+++ myprotein/photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+#djang.views.geneticからTemplateViewをインポート
+from django.vies.genetic import TemplateView
 
+class IndexView(TemplateView):
+    #トップページのビュー
+#index.htmlをレンダリングする
+template_name = 'index.html'
 # Create your views here.
